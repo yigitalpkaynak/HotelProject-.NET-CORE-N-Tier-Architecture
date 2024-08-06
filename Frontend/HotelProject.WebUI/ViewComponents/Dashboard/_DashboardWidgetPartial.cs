501f6a2ca0b6d5240f3729ae238954839b9954e1
Yigit <manwithtuned@gmail.com>
2024-08-06 19:28:10 +0300

    StaffCount added

--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardWidgetPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardWidgetPartial.cs
@@ -1,9 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.GuestDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
     public class _DashboardWidgetPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() { return View(); }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardWidgetPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:36347/api/DashboardWidgets/StaffCount");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                //var values = JsonConvert.DeserializeObject<List<ResultGuestDto>>(jsonData);
+                ViewBag.v = jsonData;
+            }
+            return View();
+        }
     }
 }
